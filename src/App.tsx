19edf32689789456d3bbadbec3e1e9c9322e60b9
Gian <gdass8000@gmail.com>
2022-10-13 23:45:15 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/Button'
+import { Heading } from './components/Heading'
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+      <Heading>Oi</Heading>
+      <Button>Botão</Button>
+    </>
+  )
+}
